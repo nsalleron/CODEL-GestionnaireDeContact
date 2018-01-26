3ed85d114d0e9d156b125e88d37574242fde6941
Nicolas SALLERON <nicolas.salleron@gmail.com>
2018-01-26 18:16:34 +0100

    Mise en page cache

--- src/services/TestsService.java
+++ src/services/TestsService.java
@@ -13,24 +13,22 @@ public class TestsService {
 		StringBuilder tmp = new StringBuilder();
 		ArrayList<String> arrayString = new ArrayList<String>();
 		
-		tmp.append("# OF HIT" + HibernateUtil.getSessionFactory().getStatistics().getSecondLevelCacheHitCount()+"<br>\n");
-		tmp.append("# OF MISS" + HibernateUtil.getSessionFactory().getStatistics().getSecondLevelCacheMissCount()+"<br>\n");
-		tmp.append("# OF PUT" + HibernateUtil.getSessionFactory().getStatistics().getSecondLevelCachePutCount()+"<br>\n");
-		
-		
 		List<CacheManager> tempManagers = CacheManager.ALL_CACHE_MANAGERS;
 		tmp.append("# of CMs : " + tempManagers.size()+"<br>\n");
+		int j = 0;
 		for (CacheManager tempCM : tempManagers) {
+			tmp.append("Cache #"+j+"<br>\n");
 			tmp.append("Name: " + tempCM.getName()+"<br>\n");
 			tmp.append("Disk path: " + tempCM.getDiskStorePath()+"<br>\n");
 			tmp.append("Status: " + tempCM.getStatus()+"<br>\n");
 			tmp.append("Cache names : <br>\n");
 			String[] cacheNames = HibernateUtil.getSessionFactory().getStatistics().getSecondLevelCacheRegionNames();
 			for (int i = 0; i < cacheNames.length; i++) {
-				 tmp.append("SECOND    -> "+cacheNames[i]+"<br>\n");
+				 tmp.append(" -> "+cacheNames[i]+"<br>\n");
 			}
+			tmp.append("<br>\n");
 			tmp.setLength(tmp.length() - 1);
-			
+			j++;
 			arrayString.add(tmp.toString());
 			System.out.println(tmp.toString());
 			tmp = new StringBuilder();
